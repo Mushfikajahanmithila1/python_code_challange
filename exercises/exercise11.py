691cc310f01b3907cd563ae3426f2bdaf3bd69d4
mushfika <musfika962@gmail.com>
2026-09-24 12:40:12 +0600

    trak

--- exercises/exercise11.py
+++ exercises/exercise11.py
@@ -3,8 +3,8 @@ from win32com.client import constants
 import win32com.client
 speaker = win32com.client.Dispatch("SAPI.SpVoice")
 while True:
-    speaker.Speak("💧 Time to drink water!")
-    # print("💧 Time to drink water!")
+    speaker.Speak("Time to drink water! Please, Drink your water, drink your water.")
+    # print("Time to drink water!")
 
     print("\a")
 
